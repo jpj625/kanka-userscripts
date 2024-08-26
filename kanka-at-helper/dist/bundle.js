a113aab9981c3193558e1533847833583c6cccb8
Jeff Johnston <jpj625@gmail.com>
2024-08-25 22:24:17 -0700

    Push 0.3.2-9 build

--- kanka-at-helper/dist/bundle.js
+++ kanka-at-helper/dist/bundle.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Kanka @ Helper (dev)
 // @namespace    https://greasyfork.org/en/users/1029479-infinitegeek
-// @version      0.3.2-8
+// @version      0.3.2-9
 // @description  Improve the experience of referencing entities.
 // @author       InfiniteGeek
 // @supportURL   Infinite @ https://discord.gg/rhsyZJ4
@@ -18,103 +18,64 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
-(() => {
-var exports = __webpack_exports__;
-var __webpack_unused_export__;
 
-var _a;
-var _b;
-__webpack_unused_export__ = ({ value: true });
-(_a = (_b = $.prototype).blink) !== null && _a !== void 0 ? _a : (_b.blink = function (times, duration) {
-    for (let i = 0; i < times; i++) {
-        this.animate({ opacity: 0 }, duration)
-            .animate({ opacity: 1 }, duration);
-    }
-    return this;
-});
-const summernoteIntercept = (event) => {
-    var _a, _b;
+const simpleIntercept = (editor, event) => {
     if (event.key !== '@') {
-        return true;
+        return;
     }
     const selection = window.getSelection();
-    console.log({ selection });
-    if (!selection || selection.rangeCount === 0) {
-        return true;
+    if (!selection || !selection.rangeCount) {
+        return;
     }
-    const range = selection.getRangeAt(0);
-    const selectedText = range.toString();
-    const modifiedText = document.createTextNode(`@${selectedText.replace(/ /g, '_')}`);
     event.preventDefault();
-    // event.stopImmediatePropagation();
-    // console.log({range, selectedText, modifiedText});
-    range.deleteContents();
-    range.insertNode(modifiedText);
-    range.collapse(false);
-    // range.setStartAfter(modifiedText);
-    // range.setEndAfter(modifiedText);
-    selection.removeAllRanges();
-    selection.addRange(range);
-    const editor = $(document.activeElement);
-    editor.summernote('insertText', modifiedText);
-    // setTimeout(() => {
-    //     const fakeevent = new KeyboardEvent('keydown', { bubbles: true, cancelable: true, key: 'ArrowRight', code: 'ArrowRight' });
-    //     document.activeElement?.dispatchEvent(fakeevent);
-    // }, 100);
-    (_a = document.activeElement) === null || _a === void 0 ? void 0 : _a.dispatchEvent(new CompositionEvent('compositionend', { bubbles: true, cancelable: true, data: '' }));
-    (_b = document.activeElement) === null || _b === void 0 ? void 0 : _b.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true, cancelable: true, key: 'ArrowRight', code: 'ArrowRight' }));
-    return false;
-};
-const simpleIntercept = (editor, event) => {
-    if (event.key === '@') {
-        event.preventDefault();
-        const selection = window.getSelection();
-        console.log({ selection });
-        if (selection && selection.rangeCount > 0) {
-            const modifiedText = '@' + selection.toString().trimEnd().replace(/ /g, '_');
-            editor.summernote('insertText', modifiedText);
-            // Simulate the right arrow key press to trigger the mentions dropdown
-            const e = new KeyboardEvent('keydown', {
-                key: 'ArrowRight',
-                keyCode: 39,
-                which: 39,
-                bubbles: true
-            });
-            editor[0].dispatchEvent(e);
-        }
+    const selectedText = selection.toString();
+    const trimmedText = selectedText.trim();
+    // handle prefix and suffix whitespace
+    if (trimmedText.length !== selectedText.length) {
+        const range = selection.getRangeAt(0);
+        const startOffset = range.startOffset + (selectedText.length - selectedText.trimStart().length);
+        const endOffset = range.endOffset - (selectedText.length - selectedText.trimEnd().length);
+        range.setStart(range.startContainer, startOffset);
+        range.setEnd(range.endContainer, endOffset);
+        selection.removeAllRanges();
+        selection.addRange(range);
     }
+    const modifiedText = '@' + trimmedText.replace(/ /g, '_');
+    editor.summernote('insertText', modifiedText);
+    // Simulate a right arrow key press to trigger the mentions dropdown
+    editor[0].dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowRight', keyCode: 39, which: 39, bubbles: true }));
 };
 function addKeydownHandler() {
     try {
+        if (!$) {
+            setTimeout(addKeydownHandler, 100);
+            return;
+        }
         const form = document.querySelector('form.entity-form');
         const editor = $(form).find('#entry.html-editor');
         if (editor.length > 0) {
             if (!!editor.summernote) {
-                editor.on('summernote.keydown', function (we, event) {
-                    console.log('Keydown event detected:', event);
+                editor.on('summernote.keydown', function (jqueryEvent, event) {
+                    // console.log('Keydown event detected:', event);
                     simpleIntercept(editor, event);
                 });
                 console.log('Keydown handler attached.');
             }
             else {
                 console.log('Editor not ready. Retrying...');
-                setTimeout(addKeydownHandler, 500); // Retry after some time
+                setTimeout(addKeydownHandler, 200);
             }
         }
         else {
             console.log('Editor not found. Retrying...');
-            setTimeout(addKeydownHandler, 500); // Retry after some time
+            setTimeout(addKeydownHandler, 200);
         }
     }
     catch (error) {
-        console.log('Broke...', error);
-        setTimeout(addKeydownHandler, 1000); // Retry after some time
+        console.log('@-Helper Broke...', error);
     }
 }
-setTimeout(addKeydownHandler, 500); // Adjust the delay as needed
-
-})();
+setTimeout(addKeydownHandler, 100);
 
 /******/ })()
 ;
--- kanka-at-helper/src/main.ts
+++ kanka-at-helper/src/main.ts
@@ -1,119 +1,64 @@
 'use strict';
 
-import Mousetrap from 'mousetrap';
 
-
-declare global {
-    type MousetrapCallback = (e: Mousetrap.ExtendedKeyboardEvent, combo: string) => void;
-
-    type Maybe<T> = NonNullable<T> | undefined;
-    type Dictionary<T> = Record<string, T>;
-    interface Window {
-        jQuery: JQueryStatic;
-        ajaxTooltip: Function;
-        showToast: (message: string, messageType: string) => void;
-    }
-    interface JQuery<TElement = HTMLElement> {
-        blink: (times: number, duration: number) => JQuery<TElement>;
-    }
-}
-
-$.prototype.blink ??= function (times: number, duration: number) {
-    for (let i = 0; i < times; i++) {
-        this.animate({ opacity: 0 }, duration)
-            .animate({ opacity: 1 }, duration);
-    }
-    return this;
-}
-
-const summernoteIntercept = (event: JQuery.TriggeredEvent) => {
-
+const simpleIntercept = (editor: JQuery<HTMLElement>, event: KeyboardEvent) => {
     if (event.key !== '@') {
-        return true;
+        return;
     }
 
     const selection = window.getSelection();
-    console.log({ selection });
-    if (!selection || selection.rangeCount === 0) { return true; }
 
-    const range = selection.getRangeAt(0);
-    const selectedText = range.toString();
-    const modifiedText = document.createTextNode(`@${selectedText.replace(/ /g, '_',)}`);
+    if (!selection || !selection.rangeCount) {
+        return;
+    }
+
     event.preventDefault();
-    // event.stopImmediatePropagation();
+    const selectedText = selection.toString();
+    const trimmedText = selectedText.trim();
 
-    // console.log({range, selectedText, modifiedText});
-    range.deleteContents();
-    range.insertNode(modifiedText);
+    // handle prefix and suffix whitespace
+    if (trimmedText.length !== selectedText.length) {
+        const range = selection.getRangeAt(0);
+        const startOffset = range.startOffset + (selectedText.length - selectedText.trimStart().length);
+        const endOffset = range.endOffset - (selectedText.length - selectedText.trimEnd().length);
+        range.setStart(range.startContainer, startOffset);
+        range.setEnd(range.endContainer, endOffset);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
 
-    range.collapse(false);
-    // range.setStartAfter(modifiedText);
-    // range.setEndAfter(modifiedText);
-    selection.removeAllRanges();
-    selection.addRange(range);
-
-    const editor = $(document.activeElement!);
+    const modifiedText = '@' + trimmedText.replace(/ /g, '_');
     editor.summernote('insertText', modifiedText);
 
-
-    // setTimeout(() => {
-    //     const fakeevent = new KeyboardEvent('keydown', { bubbles: true, cancelable: true, key: 'ArrowRight', code: 'ArrowRight' });
-    //     document.activeElement?.dispatchEvent(fakeevent);
-    // }, 100);
-    document.activeElement?.dispatchEvent(new CompositionEvent('compositionend', { bubbles: true, cancelable: true, data: '' }));
-    document.activeElement?.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true, cancelable: true, key: 'ArrowRight', code: 'ArrowRight' }));
-
-    return false;
-};
-
-const simpleIntercept = (editor: JQuery<HTMLElement>, event: JQuery.TriggeredEvent) => {
-    if (event.key === '@') {
-        event.preventDefault();
-        const selection = window.getSelection();
-
-        console.log({ selection });
-
-        if (selection && selection.rangeCount > 0) {
-            const modifiedText = '@' + selection.toString().trimEnd().replace(/ /g, '_');
-            editor.summernote('insertText', modifiedText);
-
-            // Simulate the right arrow key press to trigger the mentions dropdown
-            const e = new KeyboardEvent('keydown', {
-                key: 'ArrowRight',
-                keyCode: 39,
-                which: 39,
-                bubbles: true
-            });
-            editor[0].dispatchEvent(e);
-        }
-    }
+    // Simulate a right arrow key press to trigger the mentions dropdown
+    editor[0].dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowRight', keyCode: 39, which: 39, bubbles: true }));
 };
 
 function addKeydownHandler() {
     try {
+        if (!$) { setTimeout(addKeydownHandler, 100); return; }
         const form = document.querySelector('form.entity-form');
         const editor = $(form!).find('#entry.html-editor');
 
         if (editor.length > 0) {
             if (!!editor.summernote) {
-                editor.on('summernote.keydown', function (we, event) {
-                    console.log('Keydown event detected:', event);
+                editor.on('summernote.keydown', function (jqueryEvent, event: KeyboardEvent) {
+                    // console.log('Keydown event detected:', event);
                     simpleIntercept(editor, event);
                 });
 
                 console.log('Keydown handler attached.');
             } else {
                 console.log('Editor not ready. Retrying...');
-                setTimeout(addKeydownHandler, 500); // Retry after some time
+                setTimeout(addKeydownHandler, 200);
             }
         } else {
             console.log('Editor not found. Retrying...');
-            setTimeout(addKeydownHandler, 500); // Retry after some time
+            setTimeout(addKeydownHandler, 200);
         }
     } catch (error) {
-        console.log('Broke...', error);
-        setTimeout(addKeydownHandler, 1000); // Retry after some time
+        console.log('@-Helper Broke...', error);
     }
 }
 
-setTimeout(addKeydownHandler, 500); // Adjust the delay as needed
+setTimeout(addKeydownHandler, 100);
