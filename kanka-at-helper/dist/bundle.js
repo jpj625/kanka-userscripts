0894dfa45050bab5761eebab44256c963cd199cf
Jeff Johnston <jpj625@gmail.com>
2024-09-03 14:36:46 -0700

    Push 0.3.2-10 build

--- kanka-at-helper/dist/bundle.js
+++ kanka-at-helper/dist/bundle.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Kanka @ Helper (dev)
 // @namespace    https://greasyfork.org/en/users/1029479-infinitegeek
-// @version      0.3.2-9
+// @version      0.3.2-10
 // @description  Improve the experience of referencing entities.
 // @author       InfiniteGeek
 // @supportURL   Infinite @ https://discord.gg/rhsyZJ4
@@ -16,66 +16,88 @@
 // ==/UserScript==
 
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 var __webpack_exports__ = {};
-
-const simpleIntercept = (editor, event) => {
-    if (event.key !== '@') {
-        return;
+System.register([], function (exports_1, context_1) {
+    'use strict';
+    var keydownHandler;
+    var __moduleName = context_1 && context_1.id;
+    function getEditorHandle() {
+        return new Promise(resolve => {
+            const asionvbon = () => {
+                if (!!$) {
+                    const form = document.querySelector('form.entity-form');
+                    if (!!form) {
+                        const editor = $(form).find('#entry.html-editor');
+                        if (!!editor) {
+                            clearInterval(sir);
+                            resolve(editor);
+                        }
+                    }
+                }
+            };
+            const sir = setInterval(asionvbon, 100);
+        });
     }
-    const selection = window.getSelection();
-    if (!selection || !selection.rangeCount) {
-        return;
-    }
-    event.preventDefault();
-    const selectedText = selection.toString();
-    const trimmedText = selectedText.trim();
-    // handle prefix and suffix whitespace
-    if (trimmedText.length !== selectedText.length) {
-        const range = selection.getRangeAt(0);
-        const startOffset = range.startOffset + (selectedText.length - selectedText.trimStart().length);
-        const endOffset = range.endOffset - (selectedText.length - selectedText.trimEnd().length);
-        range.setStart(range.startContainer, startOffset);
-        range.setEnd(range.endContainer, endOffset);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-    const modifiedText = '@' + trimmedText.replace(/ /g, '_');
-    editor.summernote('insertText', modifiedText);
-    // Simulate a right arrow key press to trigger the mentions dropdown
-    editor[0].dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowRight', keyCode: 39, which: 39, bubbles: true }));
-};
-function addKeydownHandler() {
-    try {
-        if (!$) {
-            setTimeout(addKeydownHandler, 100);
-            return;
-        }
-        const form = document.querySelector('form.entity-form');
-        const editor = $(form).find('#entry.html-editor');
-        if (editor.length > 0) {
-            if (!!editor.summernote) {
-                editor.on('summernote.keydown', function (jqueryEvent, event) {
-                    // console.log('Keydown event detected:', event);
-                    simpleIntercept(editor, event);
-                });
-                console.log('Keydown handler attached.');
+    async function addKeydownHandler() {
+        try {
+            const editor = await getEditorHandle();
+            if (!editor) {
+                console.log('Editor not found. Retrying...');
+                setTimeout(addKeydownHandler, 200);
+                return;
             }
-            else {
+            if (!editor.summernote) {
                 console.log('Editor not ready. Retrying...');
                 setTimeout(addKeydownHandler, 200);
+                return;
             }
+            editor.on('summernote.keydown', function (jqueryEvent, event) {
+                return keydownHandler(editor, event);
+            });
+            console.log('Keydown handler attached.');
         }
-        else {
-            console.log('Editor not found. Retrying...');
-            setTimeout(addKeydownHandler, 200);
+        catch (error) {
+            console.log('@-Helper Broke...', error);
         }
     }
-    catch (error) {
-        console.log('@-Helper Broke...', error);
-    }
-}
-setTimeout(addKeydownHandler, 100);
+    return {
+        setters: [],
+        execute: async function () {
+            keydownHandler = (editor, event) => {
+                const AT = '@';
+                if (event.key !== AT) {
+                    return true;
+                }
+                const selection = window.getSelection();
+                if (!selection || !selection.rangeCount) {
+                    return;
+                }
+                event.preventDefault();
+                const selectedText = selection.toString();
+                const trimmedText = selectedText.trim();
+                // handle prefix and suffix whitespace
+                if (trimmedText.length !== selectedText.length) {
+                    const range = selection.getRangeAt(0);
+                    console.log({ selection, range, trimmedText });
+                    // necessary to pull selection back to not cross boundaries
+                    if (range.startContainer !== range.endContainer)
+                        range.setEnd(range.startContainer, trimmedText.length);
+                    const startOffset = range.startOffset + (selectedText.length - selectedText.trimStart().length);
+                    const endOffset = range.endOffset - (selectedText.length - selectedText.trimEnd().length);
+                    range.setStart(range.startContainer, startOffset);
+                    range.setEnd(range.endContainer, endOffset);
+                    selection.removeAllRanges();
+                    selection.addRange(range);
+                }
+                const modifiedText = AT + trimmedText.replace(/ /g, '_');
+                editor.summernote('insertText', modifiedText);
+                // Simulate a right arrow key press to trigger the mentions dropdown
+                editor[0].dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowRight', keyCode: 39, which: 39, bubbles: true }));
+            };
+            await addKeydownHandler();
+        }
+    };
+});
 
 /******/ })()
 ;
--- kanka-at-helper/src/main.ts
+++ kanka-at-helper/src/main.ts
@@ -1,9 +1,11 @@
 'use strict';
 
 
-const simpleIntercept = (editor: JQuery<HTMLElement>, event: KeyboardEvent) => {
-    if (event.key !== '@') {
-        return;
+const keydownHandler = (editor: JQuery<HTMLElement>, event: KeyboardEvent) => {
+    const AT = '@';
+
+    if (event.key !== AT) { 
+        return true; 
     }
 
     const selection = window.getSelection();
@@ -19,46 +21,73 @@ const simpleIntercept = (editor: JQuery<HTMLElement>, event: KeyboardEvent) => {
     // handle prefix and suffix whitespace
     if (trimmedText.length !== selectedText.length) {
         const range = selection.getRangeAt(0);
+        console.log({selection, range, trimmedText});
+
+        // necessary to pull selection back to not cross boundaries
+        if (range.startContainer !== range.endContainer) range.setEnd(range.startContainer, trimmedText.length);
+
         const startOffset = range.startOffset + (selectedText.length - selectedText.trimStart().length);
         const endOffset = range.endOffset - (selectedText.length - selectedText.trimEnd().length);
+
         range.setStart(range.startContainer, startOffset);
         range.setEnd(range.endContainer, endOffset);
+
         selection.removeAllRanges();
         selection.addRange(range);
     }
 
-    const modifiedText = '@' + trimmedText.replace(/ /g, '_');
+    const modifiedText = AT + trimmedText.replace(/ /g, '_');
     editor.summernote('insertText', modifiedText);
 
     // Simulate a right arrow key press to trigger the mentions dropdown
     editor[0].dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowRight', keyCode: 39, which: 39, bubbles: true }));
 };
 
-function addKeydownHandler() {
-    try {
-        if (!$) { setTimeout(addKeydownHandler, 100); return; }
-        const form = document.querySelector('form.entity-form');
-        const editor = $(form!).find('#entry.html-editor');
-
-        if (editor.length > 0) {
-            if (!!editor.summernote) {
-                editor.on('summernote.keydown', function (jqueryEvent, event: KeyboardEvent) {
-                    // console.log('Keydown event detected:', event);
-                    simpleIntercept(editor, event);
-                });
-
-                console.log('Keydown handler attached.');
-            } else {
-                console.log('Editor not ready. Retrying...');
-                setTimeout(addKeydownHandler, 200);
+function getEditorHandle(): Promise<JQuery<HTMLElement>> {
+    return new Promise(resolve => {
+        const asionvbon = () => {
+            if (!!$) {
+                const form = document.querySelector('form.entity-form');
+                if (!!form) {
+                    const editor = $(form!).find('#entry.html-editor');
+                    if (!!editor) {
+                        clearInterval(sir);
+                        resolve(editor);
+                    }
+                }
             }
-        } else {
+        };
+
+        const sir = setInterval(asionvbon, 100);
+    });
+}
+
+async function addKeydownHandler() {
+    try {
+        const editor = await getEditorHandle();
+
+        if (!editor) {
             console.log('Editor not found. Retrying...');
             setTimeout(addKeydownHandler, 200);
+            return;
         }
+
+        if (!editor.summernote) {
+            console.log('Editor not ready. Retrying...');
+            setTimeout(addKeydownHandler, 200);
+            return;
+        }
+
+        editor.on('summernote.keydown', function (jqueryEvent, event: KeyboardEvent) {
+            return keydownHandler(editor, event);
+        });
+
+        console.log('Keydown handler attached.');
     } catch (error) {
         console.log('@-Helper Broke...', error);
     }
 }
 
-setTimeout(addKeydownHandler, 100);
+await addKeydownHandler();
+
+export { };
