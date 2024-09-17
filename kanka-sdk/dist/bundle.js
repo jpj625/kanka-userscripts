79a507de0eb232fb9a24004e55bb082972f1a221
Jeff Johnston <jpj625@gmail.com>
2024-09-17 02:32:51 -0700

    Push 0.0.1-2 build

--- kanka-sdk/dist/bundle.js
+++ kanka-sdk/dist/bundle.js
@@ -1,75 +1,71 @@
-'use strict';
 
-type EntityType = 'default' | 'character' | 'creature' | 'event' | 'family' | 'item'
-    | 'journal' | 'location' | 'map' | 'note' | 'organisation' | 'quest' | 'race' | 'tag';
+// ==UserScript==
+// @name         Kanka SDK (dev)
+// @namespace    https://greasyfork.org/en/users/1029479-infinitegeek
+// @version      0.0.1-2
+// @description  Tools for Kanking.
+// @author       InfiniteGeek
+// @supportURL   Infinite @ https://discord.gg/rhsyZJ4
+// @license      MIT
+// @match        https://app.kanka.io/w/*
+// @icon         https://www.google.com/s2/favicons?domain=kanka.io
+// @keywords     kanka,sdk
+// @grant        none
+// ==/UserScript==
 
-declare global {
-    type Maybe<T> = NonNullable<T> | undefined;
-    type Dictionary<T> = Record<string, T>;
-    type Thing = { entityType: string, id: string };
-    type Entity = Thing & { entityType: EntityType, type: string };
-    type toastBackground = 'bg-primary' | 'bg-secondary' | 'bg-success' | 'bg-accent' | 'bg-neutral' | 'bg-warning' | 'bg-error';
-    type toastText = 'text-primary-content' | 'text-success-content' | 'text-accent-content' | 'text-neutral-content' | 'text-warning-content' | 'text-error-content';
-    type toastClasses = `${toastBackground} ${toastText}`
-    interface Window {
-        jQuery: JQueryStatic;
-        ajaxTooltip: Function;
-        /**
-         * @param message 
-         * @param messageType 
-         * @returns 
-         */
-        showToast: (message: string, messageType: 'error' | toastClasses | null) => void;
-    }
-}
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
+var __webpack_unused_export__;
 
-const emit_debug = (...args: any[]) => { };
+var _a, _b;
+__webpack_unused_export__ = ({ value: true });
+const emit_debug = (...args) => { };
 //const emit_debug = console.log;
-
-function getElementPromise(...selectorChain: string[]): Promise<JQuery<HTMLElement>> {
-    let intervalHandle: NodeJS.Timeout;
-    let doc: JQuery<HTMLElement>;
+function getElementPromise(...selectorChain) {
+    let intervalHandle;
+    let doc;
     return new Promise((resolve, reject) => {
-
         const getElement = () => {
-            if (!jQuery) return undefined;
-
+            if (!jQuery)
+                return undefined;
             try {
-                let lmnt: JQuery<HTMLElement> = (doc ??= jQuery(document.documentElement));
-
+                let lmnt = (doc !== null && doc !== void 0 ? doc : (doc = jQuery(document.documentElement)));
                 const selectors = [...selectorChain];
                 let selector = null;
                 while (selector = selectors.shift()) {
                     lmnt = lmnt.find(selector);
-                    if (!lmnt) return undefined;
+                    if (!lmnt)
+                        return undefined;
                 }
-
-                if (!lmnt) return null;
-
+                if (!lmnt)
+                    return null;
                 intervalHandle && clearInterval(intervalHandle);
                 resolve(lmnt);
                 return lmnt;
-
-            } catch (error) {
+            }
+            catch (error) {
                 intervalHandle && clearInterval(intervalHandle);
                 reject(error);
                 return null;
             }
         };
-
         if (typeof MutationObserver) {
             // if we have the MutationObserver API, hook to document changes
             const observer = new MutationObserver(() => getElement() && observer.disconnect());
             observer.observe(document.documentElement, { childList: true, subtree: true });
-        } else {
+        }
+        else {
             // if not, use a sad timer
             intervalHandle = setInterval(getElement, 333);
         }
     });
 }
-
 const Api = {
-    getXMLHttpRequest: (method: 'GET' | 'POST' | string): XMLHttpRequest => {
+    getXMLHttpRequest: (method) => {
         var xhr = new XMLHttpRequest();
         xhr.withCredentials = true;
         xhr.open(method, Uri.buildUri(Entity.entityType, Entity.typedID), false);
@@ -77,13 +73,11 @@ const Api = {
         Api.headers.setXMLHttpRequest(xhr);
         return xhr;
     },
-
     headers: {
-        setCsrf: (xhr: XMLHttpRequest) => xhr.setRequestHeader('x-csrf-token', Session.csrfToken),
-        setXMLHttpRequest: (xhr: XMLHttpRequest) => xhr.setRequestHeader('x-requested-with', 'XMLHttpRequest'),
+        setCsrf: (xhr) => xhr.setRequestHeader('x-csrf-token', Session.csrfToken),
+        setXMLHttpRequest: (xhr) => xhr.setRequestHeader('x-requested-with', 'XMLHttpRequest'),
     },
-
-    createPostParams: (): URLSearchParams => {
+    createPostParams: () => {
         const params = new URLSearchParams();
         params.append('_token', Session.csrfToken);
         params.append('datagrid-action', 'batch');
@@ -95,16 +89,13 @@ const Api = {
         params.append('undefined', '');
         return params;
     },
-
-
-    fetch_success: async (response: Response): Promise<{ ok: boolean, document: JQuery.Node[] }> => {
+    fetch_success: async (response) => {
+        var _a;
         emit_debug('Success:', response);
         window.showToast(response.statusText, 'bg-success text-success-content');
-        return { ok: response.ok, document: $.parseHTML(await response.text()) ?? [] };
+        return { ok: response.ok, document: (_a = $.parseHTML(await response.text())) !== null && _a !== void 0 ? _a : [] };
     },
-
-    post: (url: string, body: URLSearchParams)
-        : Promise<{ ok: boolean, document: JQuery.Node[], error?: unknown }> => {
+    post: (url, body) => {
         return fetch(url, {
             method: 'POST',
             redirect: 'follow',
@@ -113,68 +104,64 @@ const Api = {
         })
             .then(Api.fetch_success)
             .catch((error) => {
-                console.error('Error:', error);
-                window.showToast(error, 'bg-primary text-error-content');
-                return { ok: false, document: [], error };
-            });
+            console.error('Error:', error);
+            window.showToast(error, 'bg-primary text-error-content');
+            return { ok: false, document: [], error };
+        });
     }
-
 };
-
 /**
  * Extract metadata from the classes on the <body>
  */
-function parseBodyClasses(body: HTMLElement): {
-    entity: Entity,
-    tags: Thing[],
-} {
+function parseBodyClasses(body) {
     const classes = Array.from(body.classList);
-
-    const entity: Entity = { id: '', entityType: 'default', type: '' };
-    const tags: Thing[] = [];
-
+    const entity = { id: '', entityType: 'default', type: '' };
+    const tags = [];
     const kankaClassRegex = /^kanka-(\w+)-(\w+)$/;
-
-    let tempTag: string | null = null;
-    function processTag(isValueNumeric: boolean, value: string) {
+    let tempTag = null;
+    function processTag(isValueNumeric, value) {
         // tags are emitted as id/name pairs
         // parent tags also end up in the list as ID-only entries
         // any name is associated with the ID prior
         if (isValueNumeric) {
             tempTag = value;
-        } else if (tempTag !== null) {
+        }
+        else if (tempTag !== null) {
             tags.push({ id: tempTag, entityType: value });
             tempTag = null;
         }
     }
-
     classes
         .map(className => className.match(kankaClassRegex))
         .filter(match => !!match)
-        .forEach((match: RegExpMatchArray) => {
-            const [, key, value] = match;
-            const isValueNumeric = !isNaN(Number(value));
-
-            switch (key) {
-                // kanka-entity-{entityID} kanka-entity-{entityType}
-                case 'entity':
-                    if (isValueNumeric) { entity['id'] = value; }
-                    else { entity['entityType'] = value as EntityType; }
-                    break;
-
-                // kanka-type-{typeValue}
-                case 'type': entity.type = value; break;
-
-                // kanka-tag-{id} kanka-tag-{name}
-                case 'tag': processTag(isValueNumeric, value); break;
-
-                default: console.warn("What's this? 💀🎃", match); break;
-            }
-        });
-
+        .forEach((match) => {
+        const [, key, value] = match;
+        const isValueNumeric = !isNaN(Number(value));
+        switch (key) {
+            // kanka-entity-{entityID} kanka-entity-{entityType}
+            case 'entity':
+                if (isValueNumeric) {
+                    entity['id'] = value;
+                }
+                else {
+                    entity['entityType'] = value;
+                }
+                break;
+            // kanka-type-{typeValue}
+            case 'type':
+                entity.type = value;
+                break;
+            // kanka-tag-{id} kanka-tag-{name}
+            case 'tag':
+                processTag(isValueNumeric, value);
+                break;
+            default:
+                console.warn("What's this? 💀🎃", match);
+                break;
+        }
+    });
     return { entity, tags };
 }
-
 /**
  * Builds a comparison function for sorting by similarity to a provided term.
  * Intended for sorting typeahead results.
@@ -185,19 +172,12 @@ term: 'tre'
     "Treasure of the Sierra Madre" => 26 (starts with, case mismatch)
     "one tree hill" => 15 (includes, start of word, case match)
  */
-function createMatchinessComparator<TItem extends Object>
-    (
-        term: string,
-        converter: (item: TItem) => string = item => item.toString()
-    )
-    : (a: TItem, b: TItem) => number {
+function createMatchinessComparator(term, converter = item => item.toString()) {
     const locale = Intl.Collator().resolvedOptions().locale;
-
     const pattern = {
         startsWith: '^' + term,
         startsWord: '\\b' + term,
     };
-
     const regex = {
         startsWith: new RegExp(pattern.startsWith),
         startsWithI: new RegExp(pattern.startsWith, 'i'),
@@ -206,89 +186,68 @@ function createMatchinessComparator<TItem extends Object>
         includes: new RegExp(term),
         includesI: new RegExp(term, 'i'),
     };
-
     // assign a score based on how well the value matches the search term
-    const computeMatchiness = (value: string) => {
+    const computeMatchiness = (value) => {
         switch (true) {
             // exact match
             case value === term: return 30;
-
             // close match, just varying by accents and/or case
             case value.localeCompare(term, locale, { usage: 'search', sensitivity: 'variant' }) === 0: return 28;
             case value.localeCompare(term, locale, { usage: 'search', sensitivity: 'accent' }) === 0: return 27;
             case value.localeCompare(term, locale, { usage: 'search', sensitivity: 'case' }) === 0: return 26;
             case value.localeCompare(term, locale, { usage: 'search', sensitivity: 'base' }) === 0: return 25;
-
             // starts with (including case-insensitive)
             case regex.startsWith.test(value): return 20;
             case regex.startsWithI.test(value): return 18;
-
             // includes at the start of a word (including case-insensitive)
             case regex.startsWord.test(value): return 15;
             case regex.startsWordI.test(value): return 13;
-
             // includes anywhere (including case-insensitive)
             case regex.includes.test(value): return 10;
             case regex.includesI.test(value): return 9;
-
             // no match
             default: return 0;
         }
     };
-
-    return (a: TItem, b: TItem): number => {
+    return (a, b) => {
         const textA = converter(a);
         const textB = converter(b);
-
         const scoreA = computeMatchiness(textA);
         const scoreB = computeMatchiness(textB);
-
-        const relativeMatchiness = Math.sign(scoreB - scoreA) as 0 | 1 | -1;
-
+        const relativeMatchiness = Math.sign(scoreB - scoreA);
         // sort by score, then alphabetically when equal
         // localeCompare impls may not be 1|0|-1 only
         return relativeMatchiness || textA.localeCompare(textB);
     };
 }
-
 const Uri = {
     rootUri: 'https://app.kanka.io',
     route: window.location.pathname,
-
-    buildUri: (...segments: string[]) => [Uri.rootUri, 'w', Session.campaignID, ...segments].join('/'),
-
-    getEditUri: () => document.querySelector('a[href$=edit]')!.getAttribute('href'),
-    getEntityUri: () => document.querySelector('head link[rel=canonical]')!.getAttribute('href'),
+    buildUri: (...segments) => [Uri.rootUri, 'w', Session.campaignID, ...segments].join('/'),
+    getEditUri: () => document.querySelector('a[href$=edit]').getAttribute('href'),
+    getEntityUri: () => document.querySelector('head link[rel=canonical]').getAttribute('href'),
 };
-
 const Session = {
-    csrfToken: document.head.querySelector('meta[name="csrf-token"]')?.getAttribute('content') as string,
-    campaignID: Uri.route.match(/w\/(?<id>\d+)\//)!.groups!.id ?? '0',
+    csrfToken: (_a = document.head.querySelector('meta[name="csrf-token"]')) === null || _a === void 0 ? void 0 : _a.getAttribute('content'),
+    campaignID: (_b = Uri.route.match(/w\/(?<id>\d+)\//).groups.id) !== null && _b !== void 0 ? _b : '0',
 };
-
-const entityBits = Uri.getEntityUri()!.match(/w\/\d+\/entities\/(?<id>\d+)/);
-const editBits = Uri.getEditUri()!.match(/\/(?<type>\w+)\/(?<id>\d+)\/edit$/);
-
+const entityBits = Uri.getEntityUri().match(/w\/\d+\/entities\/(?<id>\d+)/);
+const editBits = Uri.getEditUri().match(/\/(?<type>\w+)\/(?<id>\d+)\/edit$/);
 const Entity = {
     /**
      *  this is the plural, not values from EntityType
      */
-    entityType: editBits!.groups!.type,
-
+    entityType: editBits.groups.type,
     /**
      *  this is the 'larger' ID: entities/__[5328807]__ === characters/1357612
      */
-    entityID: entityBits!.groups!.id,
-
+    entityID: entityBits.groups.id,
     /**
      * this is the 'smaller' ID: entities/5328807 === characters/__[1357612]__
      */
-    typedID: editBits!.groups!.id,
-
+    typedID: editBits.groups.id,
     meta: parseBodyClasses(document.body),
-
 };
-
 const EntityTypeAttributes = {
     /**
      * this encapsulates the definitions from the system
@@ -311,20 +270,14 @@ const EntityTypeAttributes = {
         tag: {},
         note: {},
         quest: {},
-    }) as Record<EntityType, {
-        headerLink?: boolean,
-        sidebarLink?: boolean,
-        multiple?: boolean,
-    }>,
+    }),
 };
-
 const Util = {
     createMatchinessComparator,
     getElementPromise,
     parseBodyClasses,
 };
-
-export default {
+__webpack_unused_export__ = {
     Uri,
     Session,
     Entity,
@@ -332,3 +285,8 @@ export default {
     Util,
     Api,
 };
+
+})();
+
+/******/ })()
+;
--- kanka-sdk/webpack.config.js
+++ kanka-sdk/webpack.config.js
@@ -49,3 +49,9 @@ module.exports = {
     ],
 };
 
+function encodeRFC3986URIComponent(str) {
+    return encodeURIComponent(str).replace(
+        /[!'()*]/g,
+        (c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`,
+    );
+}
